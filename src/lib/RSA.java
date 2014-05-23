c7649c7fcf437bcb9ac27297871e121ee3b70dcf
macartur <macartur.sc@gmail.com>
2014-05-23 15:20:09 -0300

    Adding RSA project

---
+++ src/lib/RSA.java
@@ -0,0 +1,18 @@
+package lib;
+
+import java.math.BigInteger;
+
+public class RSA {
+
+	public RSA() {	
+	}
+	
+	public BigInteger generateKey(){
+		return new BigInteger("232938293829");
+	}
+	
+	public BigInteger gcd(BigInteger a, BigInteger b){
+		return new BigInteger("232938293829");			
+	}
+	
+}
---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import lib.RSA;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		RSA rsa = new RSA(); 		
+		
+	}
+
+}
