2d570e8b81d45a45464a4ee5b4b702286b035fe6
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2014-06-08 19:27:11 -0300

    Updating value of p e q
    Big Prime numbers

--- src/lib/PseudoRandomGenerator.java
+++ src/lib/PseudoRandomGenerator.java
@@ -11,17 +11,19 @@ public class PseudoRandomGenerator {
 	public PseudoRandomGenerator(String stringSeed){
 		this.seed = new BigInteger(stringSeed);
 		
-		BigInteger p = new BigInteger("10359947");
-		BigInteger q = new BigInteger("10359991");
+		BigInteger p = new BigInteger("16683587");
+		BigInteger q = new BigInteger("16683619");
 		
 		this.m = p.multiply(q);
 	}
 	
-	public void generate(){
+	public BigInteger generate(){
 		this.seed = this.seed.multiply(this.seed);
 		this.seed = this.seed.mod(this.m);
 		
 		System.out.println("Resultado: " + this.seed);
+		
+		return this.seed;
 	}
 }
 
